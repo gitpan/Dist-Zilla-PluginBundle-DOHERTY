6812fc47f68467f422f69adfbeadd0125e03c95a
Mike Doherty <doherty@cpan.org>
2011-01-11 22:06:03 +0000

    Import of DOHERTY/Dist-Zilla-PluginBundle-DOHERTY-0.005 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-PluginBundle-DOHERTY
    gitpan-cpan-version:      0.005
    gitpan-cpan-path:         DOHERTY/Dist-Zilla-PluginBundle-DOHERTY-0.005.tar.gz
    gitpan-cpan-author:       DOHERTY
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/PluginBundle/DOHERTY.pm
+++ lib/Dist/Zilla/PluginBundle/DOHERTY.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::PluginBundle::DOHERTY;
 BEGIN {
-  $Dist::Zilla::PluginBundle::DOHERTY::VERSION = '0.004';
+  $Dist::Zilla::PluginBundle::DOHERTY::VERSION = '0.005';
 }
 # ABSTRACT: configure Dist::Zilla like DOHERTY
 
@@ -165,7 +165,7 @@ Dist::Zilla::PluginBundle::DOHERTY - configure Dist::Zilla like DOHERTY
 
 =head1 VERSION
 
-version 0.004
+version 0.005
 
 =head1 SYNOPSIS
 
